f8c1897d17fb341b6ca54be486e357226741576c
zhangchao <1592727414@qq.com>
2021-05-12 20:11:58 +0800

    期末作业初步布局

---
+++ src/page/index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $.fn.fullpage({
+		// slidesColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#f90'],
+		anchors: ['page1', 'page2', 'page3', 'page4'],
+        navigation:true,
+        //设置滚动的速度
+        scrollSpeed:500,
+        loopBottom:true,
+        //单个屏幕内可滚动
+        // scrollOverflow:true,
+        //滚动前的回调函数
+        onLeave:function(index,nextIndex){
+            if(index==1){
+                $(".test").removeClass('rollIn animated');
+                $(".test").addClass('animated fadeOut');
+            }
+        },
+        // 滚动到某一屏后的回调函数
+        afterLoad:function(anchorLink,index){
+            if(index==1){
+                $(".test").removeClass('animated fadeOut');
+                $(".test").addClass('rollIn animated');
+            }
+        }
+	});
+})
